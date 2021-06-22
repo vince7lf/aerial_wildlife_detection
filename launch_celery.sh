ed99a61c138a07dde05940d2f81e1d0872aab006
Ubuntu <ubuntu@tes2.arbutus>
2021-06-22 02:28:06 +0000

    no-cache-dir in docker file; --loglevel and log file in celery

--- launch_celery.sh
+++ launch_celery.sh
@@ -24,7 +24,7 @@ if [ $launchCeleryBeat ]; then
     # folder watching interval specified; enable Celery beat
 	tempDir="$(python util/configDef.py --section=FileServer --parameter=tempfiles_dir --fallback=/tmp)/aide/celery/";
     mkdir -p $tempDir;
-    celery -A celery_worker worker -B -s $tempDir --hostname aide@%h
+    celery -A celery_worker worker -B -s $tempDir --hostname aide@%h --loglevel INFO -f /app/logs/celery_worker.log
 else
-	celery -A celery_worker worker --hostname aide@%h
+	celery -A celery_worker worker --hostname aide@%h --loglevel INFO -f /app/logs/celery_worker.log
 fi
