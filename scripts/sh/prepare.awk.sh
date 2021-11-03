48a7a3783f3a294827ba888863bc1aadd58d6506
Vincent Le Falher <vincent.lefalher@bell.ca>
2021-11-02 23:49:43 -0400

    Update Dockerfile; prepare json with all info; result file

--- scripts/sh/prepare.awk.sh
+++ scripts/sh/prepare.awk.sh
@@ -1,3 +1,41 @@
-awk -F'\t' '{print $2"\t"$3}' Liste\ Sp\ vasculaire\ Qc\ VASCAN.txt | sort | uniq | awk -F"\t" 'BEGIN{print "{"}; { if
- (!type) { type=$1; print "\""$1"\": ["} else if (type!=$1) { print "],\""$1"\": ["; type=$1; } else {print ","}}; { pri
-nt "\t{\"name\":\""$2"\", \"color\":\"#1111FF\"}" };END {print "]}"}' > classes_melcc_org.json
+# extract id
+awk -F'\t' '{
+  # http://data.canadensys.net/vascan/taxon/1142
+  match($1, /[0-9]+$/, id);
+};{
+  print id[0];
+}' test_regex_awk.txt
+
+# full command
+awk -F'\t' '{
+  # http://data.canadensys.net/vascan/taxon/1142
+  match($1, /[0-9]+$/, _res);
+  id=_res[0]
+};{
+  print id"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7;
+}' Liste\ Sp\ vasculaire\ Qc\ VASCAN.txt | sort | uniq | awk -F"\t" \
+'BEGIN {
+  print "{"
+}; {
+  if (!type) {
+    type=$2;
+    print "\""$2"\": ["
+  } else if (type!=$2) {
+    print "],\""$2"\": [";
+    type=$2;
+  } else {
+    print ","
+  }
+}; {
+  print "\t{ \
+    \"name\":\""$3"\", \
+    \"color\":\"#095797\", \
+    \"external_id\": \""$1"\", \
+    \"alias_fr\": \""$4"\", \
+    \"alias_en\": \""$5"\", \
+    \"cat1\": \""$6"\", \
+    \"cat2\": \""$7"\", \
+  }"
+};END {
+  print "]}"
+}' > classes_melcc_org_ext.json
