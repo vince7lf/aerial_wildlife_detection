0c8a3137f3947345871d89fa445754ff13a37d3a
Vincent Le Falher <vincent.lefalher@usherbrooke.ca>
2022-10-03 13:31:25 -0400

    (AIDE+MELCC#56) test if need to process geolocalized geojson. If no GPS in the Exif metadata, do not create mapserver geojson.

--- scripts/sh/gdalogr_createtiles.sh
+++ scripts/sh/gdalogr_createtiles.sh
@@ -9,7 +9,13 @@
 
 # -x to exit immediately when an error occured
 # -e to display more information
-# set -x
+DEBUG=$4
+set -ex
+devnull=/dev/null
+[ "$DEBUG" = true ] && set -ex
+# trick to cut the errors/warnings in the output of the command : redirect to /dev/null and then copy the error output 2 to standard output 1; syntax is: command > /dev/null 2>&1
+[ "$DEBUG" = true ] && devnull=1 # when debug is true, need to specify an output file descriptor to replace /dev/null; in that case we replace /dev/null by standard output 1
+# In that case the syntax is: command > 1 2>&1. The 2>&1 is important in case the command output errors/warnings we do not want to display
 # set echo off
 
 # imgFilename='test_266_tile.jpg'
@@ -26,6 +32,9 @@ geojsonFilename="${filename}.geojson"
 # clean
 _clean()
 {
+  rm -rf ${srcDir}/*.wld
+  rm -rf ${srcDir}/*.jgw
+
   rm -rf ${destDir}/*.tif
   rm -rf ${destDir}/*.wld
   rm -rf ${destDir}/*.prj
@@ -50,18 +59,18 @@ cp -rap ${srcDir}/${imgFilename} ${destDir}
 
 # create tile as shapefile
 # output and error piped to /dev/null
-gdal_retile.py -levels 1 -tileIndex ${shpFilename} -tileIndexField Location -ps 128 128 -targetDir ${destDir} ${srcDir}/${imgFilename} > /dev/null 2>&1
+gdal_retile.py SRC_METHOD=NO_GEOTRANSFORM -levels 1 -tileIndex ${shpFilename} -tileIndexField Location -ps 128 128 -targetDir ${destDir} ${srcDir}/${imgFilename}
 
 # create .geojson and generate .tiff tiles
 # output and error piped to /dev/null
-ogr2ogr -f GeoJSON -s_srs crs:84 -t_srs crs:84 ${destDir}/${geojsonFilename} ${destDir}/${shpFilename} > /dev/null 2>&1
+ogr2ogr -f GeoJSON -s_srs crs:84 -t_srs crs:84 ${destDir}/${geojsonFilename} ${destDir}/${shpFilename} > ${devnull} 2>&1
 
 # loop through the .tiff files and convert to .jpg
 tiles=()
 for f in ${destDir}/*.tif; do
   jpg="${f%.*}.jpg"
   # output and error piped to /dev/null
-  gdal_translate -of JPEG -co worldfile=yes ${f} ${jpg} > /dev/null 2>&1
+  gdal_translate -of JPEG -co worldfile=yes ${f} ${jpg} > ${devnull} 2>&1
   tiles+=(${parentDir}/${filename}/$(basename -- "${jpg}"))
 done
 
@@ -78,6 +87,16 @@ for tile in "${tiles[@]}"
 do
     echo $tile
 done
+
+_testJPEGExifGPS()
+{
+  [[ ! -z $(exiftool -s -s -s -c '%.13f' -gpslongitude ${srcDir}/${imgFilename}) ]] || { echo ""; return; };
+  [[ ! -z $(exiftool -s -s -s -c '%.13f' -gpslatitude ${srcDir}/${imgFilename}) ]] || { echo ""; return; };
+  echo 1
+}
+
+[[ "${extension,,}" =~ "jpeg"|"jpg" && ! -z $(_testJPEGExifGPS) ]] || exit 1
+#[[ "${extension,,}" =~ "tif"|"tiff" ]] && _convertTIFFToJPEG
 
 # trigger the script to create the geolocalized geojson and tiles/images and used by mapserver.
 # in the futur that script should be the only one and openlayer in the annotation interface should suppoer geolocalized geojson
