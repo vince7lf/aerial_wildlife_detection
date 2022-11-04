88cdf77a8ebcdb7f11ee3fdf9826c66205dd23b8
Vincent Le Falher <vincent.lefalher@usherbrooke.ca>
2022-11-03 22:51:39 -0400

    (AIDE+MELCC#71) fixes

--- modules/DataAdministration/backend/dataWorker.py
+++ modules/DataAdministration/backend/dataWorker.py
@@ -410,12 +410,12 @@ class DataWorker:
 
         tilenames = []
         # with open("/tmp/gdalogr_createtiles.log", "w") as f:
-        proc = subprocess.run(["gdalogr_createtiles.sh", image, destFolder, parent, project],
+        proc = subprocess.run(["gdalogr_createtiles.sh", image, destFolder, parent, project, "false"],
                               stdin=subprocess.DEVNULL,
-                              # stdout=f,
-                              stdout=subprocess.PIPE,
-                              # stderr=f,
-                              stderr=subprocess.DEVNULL,
+                              stdout=f,
+                              # stdout=subprocess.PIPE,
+                              stderr=f,
+                              # stderr=subprocess.DEVNULL,
                               universal_newlines=True,
                               bufsize=0)
 
