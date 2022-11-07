998d2a4231bf46f2cb448c9f373dadf1d9b6e318
Vincent Le Falher <vincent.lefalher@usherbrooke.ca>
2022-11-06 23:13:43 -0500

    (AIDE+MELCC#51) wip

--- tests/docker/mapserver_apache2/test.sh
+++ tests/docker/mapserver_apache2/test.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -x
 
-echo ${PYTHONPATH}
-echo ${AIDE_CONFIG_PATH}
-echo ${AIDE_MODULES}
-echo ${AIDE_ENV}
-
-sudo cp -fap /home/aide/app/docker/settings@${AIDE_ENV}.ini ${AIDE_CONFIG_PATH}
-
+echo "started"
