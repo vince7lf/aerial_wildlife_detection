d8fc943b8a5ef63cfdd615992a51f274b1934422
Vincent Le Falher <vincent.lefalher@usherbrooke.ca>
2022-11-14 15:41:18 -0500

    (AIDE+MELCC#79) added scripts/js/share_annotation.js (nodejs script) docker/Dockerfile : new comment to avoid error "gpg: can't connect (...): IPC connect call failed" and gpg-agent daemon already running

---
+++ scripts/js/share_annotations.js
@@ -0,0 +1,37 @@
+const http = require('node:http');
+
+let server_port = 'http://localhost:7780'
+let request_url = '/test-mapserver6/requestDownload'
+
+const postData = JSON.stringify({"dataType": "annotation", "extra_fields": {"meta": false}});
+
+const options = {
+    hostname: 'localhost',
+    port: 7780,
+    path: request_url,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Content-Length': Buffer.byteLength(postData)
+    }
+};
+
+const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+});
+
+req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+});
+
+// Write data to request body
+req.write(postData);
+req.end();
