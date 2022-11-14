69567180ada8d0b2c2bc1ecaf7f5f6fa6ac3e6f7
Vincent Le Falher <vincent.lefalher@usherbrooke.ca>
2022-11-14 16:39:10 -0500

    (AIDE+MELCC#79) wip

--- scripts/js/share_annotations.js
+++ scripts/js/share_annotations.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+const postDataDownloadAnnotations = JSON.stringify({"dataType": "annotation", "extra_fields": {"meta": false}});
 const reqDownloadAnnotations = http.request({
     hostname: 'localhost', port: 8080, path: '/test-mapserver6/requestDownloadAnnotations', method: 'POST', headers: {
-        'Content-Type': 'application/json; charset=UTF-8', 'Content-Length': Buffer.byteLength(postData)
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Content-Length': Buffer.byteLength(postDataDownloadAnnotations)
     }
 }, (res) => {
     console.log(`STATUS: ${res.statusCode}`);
@@ -17,6 +19,27 @@ const reqDownloadAnnotations = http.request({
     res.on('end', () => {
         console.log('No more data in response.');
         const postData = JSON.stringify({"taskID": body.response});
+        const reqPollStatus = http.request({
+            hostname: 'localhost', port: 8080, path: '/test-mapserver6/pollStatus', method: 'POST', headers: {
+                'Content-Type': 'application/json; charset=UTF-8', 'Content-Length': Buffer.byteLength(postData)
+            }
+        }, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                console.log(`BODY: ${chunk}`);
+                //
+            });
+            res.on('end', () => {
+                console.log('No more data in response.');
+            });
+        });
+
+        reqPollStatus.on('error', (e) => {
+            console.error(`problem with request: ${e.message}`);
+        });
+
         reqPollStatus.write(postData);
         reqPollStatus.end();
     });
@@ -26,29 +49,7 @@ reqDownloadAnnotations.on('error', (e) => {
     console.error(`problem with request: ${e.message}`);
 });
 
-const reqPollStatus = http.request({
-    hostname: 'localhost', port: 8080, path: '/test-mapserver6/pollStatus', method: 'POST', headers: {
-        'Content-Type': 'application/json; charset=UTF-8', 'Content-Length': Buffer.byteLength(postData)
-    }
-}, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-        console.log(`BODY: ${chunk}`);
-        //
-    });
-    res.on('end', () => {
-        console.log('No more data in response.');
-    });
-});
-
-reqPollStatus.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-});
-
 // Write data to request body
-const postData = JSON.stringify({"dataType": "annotation", "extra_fields": {"meta": false}});
-reqDownloadAnnotations.write(postData);
+reqDownloadAnnotations.write(postDataDownloadAnnotations);
 reqDownloadAnnotations.end();
 
