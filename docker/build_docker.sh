bdc8f8fa73272ee46e9e461fbe7a8da94bd5f735
Vincent Le Falher <vincent.lefalher@gmail.com>
2023-06-15 18:06:56 -0400

    set config files based on env; build based on env and set the version

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -ex
 
+git checkout -f
+
 # aide_env=lefoai
 # tag=v3.0.0b20230615T1610EST
 aide_env=$1
 tag=$2
 
-git fetch
-git pull
-git checkout tag
+git checkout ${tag}
 
 rm docker-compose.yml
 mv docker-compose@${aide_env}.yml docker-compose.yml
 rm Dockerfile
 mv Dockerfile@${aide_env} Dockerfile
 
-AIDE_VERSION=${tag} AIDE_ENV=${aide_env} sudo -E docker-compose build
+AIDE_VERSION=${tag} AIDE_ENV=${aide_env} sudo -E docker compose build
 
 # reset all changes to avoid pb next time to pull
-git chekckout -f
+git checkout -f
