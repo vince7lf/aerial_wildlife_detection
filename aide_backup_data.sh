b0369fd2e0aa157c402f7105973a6e651e0c8d91
Vincent Le Falher <vincent.lefalher@gmail.com>
2023-06-15 22:19:41 -0400

    hardcode database name aidev3

--- aide_backup_data.sh
+++ aide_backup_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-sudo -u postgres pg_dump -Fc -d ${AIDE_DB} > /home/aide/app/backup/${AIDE_ENV}-${AIDE_VERSION}-${AIDE_DB}-`date +%Y%m%dT%H%M%S`.dump
+sudo -u postgres pg_dump -Fc -d aidev3 > /home/aide/app/backup/${AIDE_ENV}-${AIDE_VERSION}-aidev3-`date +%Y%m%dT%H%M%S`.dump
