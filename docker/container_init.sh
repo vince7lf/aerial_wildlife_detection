1fff64792bd34b163cf837e8dd86a8f3696a822d
Vincent Le Falher <vincent.lefalher@gmail.com>
2023-06-20 14:43:39 -0400

    mkdir /home/aide/app/backup

--- docker/container_init.sh
+++ docker/container_init.sh
@@ -49,6 +49,7 @@ sudo systemctl enable postgresql.service
 sudo service postgresql start
 
 # setup the cronjob to backup the database
+mkdir -p /home/aide/app/backup
 (sudo crontab -u root -l 2>/dev/null; echo "* 2 * * * sudo /bin/bash /usr/local/sbin/aide_backup_data.sh 2>&1 | tee /var/log/aide_backup_data.sh-$(date +%Y%m%dT%H%M%S).log") | sudo crontab -u root -
 
 echo "=============================="
