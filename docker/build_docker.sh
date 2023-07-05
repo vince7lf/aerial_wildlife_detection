91809c8cdbef57b7ddfc82cd1415f91daab4ea48
Vincent Le Falher <vincent.lefalher@gmail.com>
2023-07-05 17:32:46 -0400

    update script to build docker

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -6,20 +6,26 @@ git checkout -f
 # aide_env=lefoai
 # tag=v3.0.0b20230615T1610EST
 # tag=v3.0-for-ivado-multienv
+# tag=latest
 # volume=aidev3
+# proxy=http://mandataire.ti.umontreal.ca:80/
 aide_env=$1
 tag=$2
 volume=$3
+proxy=$4
 
 git checkout ${tag}
 git pull
 
-rm docker-compose.yml
-mv docker-compose@${aide_env}.yml docker-compose.yml
-rm Dockerfile
-mv Dockerfile@${aide_env} Dockerfile
-
-AIDE_VERSION=${tag} AIDE_ENV=${aide_env} VOLUME_VERSION=${volume} sudo -E docker compose -f docker-compose.yml build
-
-# reset all changes to avoid pb next time to pull
-git checkout -f
+# AIDE_VERSION=${tag} AIDE_ENV=${aide_env} VOLUME_VERSION=${volume} sudo -E docker compose -f docker-compose.yml build
+AIDE_VERSION=${tag} \
+  AIDE_ENV=${aide_env} \
+  VOLUME_VERSION=${volume} \
+  sudo -E docker build \
+  --tag=aidev3_app:${tag}
+  --rm \
+  --no-cache \
+  --build-arg HTTP_PROXY=${proxy} \
+  --build-arg HTTPS_PROXY=${proxy} \
+  --file Dockerfile@${aide_env} \
+  ..
