09aa0d4051b5afd11c17a274c084e0e2b0caf133
Vincent Le Falher <vincent.le.falher@umontreal.ca>
2023-07-06 12:49:57 -0400

    improvement; use system ENV variables AIDE_ENV and HTTP_PROXY/HTTPS_PROXY; set LATEST_TAG and DEFAULT_BRANCH variables

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 set -ex
 
-git pull origin v3.0-for-ivado-multienv
-git checkout -f v3.0-for-ivado-multienv
+DEFAULT_BRANCH=v3.0-for-ivado-multienv
+LATEST_TAG=latest
+
+git pull origin ${DEFAULT_BRANCH}
+git checkout -f ${DEFAULT_BRANCH}
 
 # aide_env=lefoai
 # tag=v3.0.0b20230615T1610EST
 # tag=v3.0-for-ivado-multienv
 # volume=aidev3
 # proxy=http://mandataire.ti.umontreal.ca:80/
-aide_env=$1
-tag=$2
-proxy=$3
 
 git fetch --all --tags
-git branch --delete tmpb_${tag} || true
-git checkout tags/${tag} -b tmpb_${tag}
 
-sudo docker rmi aidev3_app:aide_latest || true
+# checkout latest tag
+git branch --delete tmpb_${LATEST_TAG} || true
+git checkout tags/${LATEST_TAG} -b tmpb_${LATEST_TAG}
+
+# read versions.txt
+source ../versions.txt
+echo ${AIDE_VERSION}
+echo ${DOCKER_VOLUME_VERSION}
+
+git branch --delete tmpb_${AIDE_VERSION} || true
+git checkout tags/${AIDE_VERSION} -b tmpb_${AIDE_VERSION}
+
+sudo docker rmi aidev3_app:aide_${LATEST_TAG} || true
 
 # AIDE_VERSION=${tag} AIDE_ENV=${aide_env} VOLUME_VERSION=${volume} sudo -E docker compose -f docker-compose.yml build
-AIDE_VERSION=${tag} \
-  AIDE_ENV=${aide_env} \
+AIDE_VERSION=${AIDE_VERSION} \
+  AIDE_ENV=${AIDE_ENV} \
   sudo -E docker build \
-  --tag=aidev3_app:aide_${tag} \
-  --tag=aidev3_app:aide_latest \
+  --tag=aidev3_app:aide_${AIDE_VERSION} \
+  --tag=aidev3_app:aide_${LATEST_TAG} \
   --rm \
   --no-cache \
-  --build-arg HTTP_PROXY=${proxy} \
-  --build-arg HTTPS_PROXY=${proxy} \
-  --file Dockerfile@${aide_env} \
+  --build-arg HTTP_PROXY=${HTTP_PROXY} \
+  --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+  --file Dockerfile@${AIDE_ENV} \
   ..
 
-git pull origin v3.0-for-ivado-multienv
-git checkout -f v3.0-for-ivado-multienv
-git branch --delete tmpb_${tag} || true
+# reset and clean
+git pull origin ${DEFAULT_BRANCH}
+git checkout -f ${DEFAULT_BRANCH}
+git branch --delete tmpb_${AIDE_VERSION} || true
+git branch --delete tmpb_${LATEST_TAG} || true
