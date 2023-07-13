dde206a75e9c8c06bf8743f6ea9f2dbee86e0e6d
Vincent Le Falher <vincent.le.falher@umontreal.ca>
2023-07-13 15:02:00 -0400

    added script to convert TIF into JPEG, add a prefix, and remove TIF

--- convert_tif_to_jpg_and_prefix.sh
+++ convert_tif_to_jpg_and_prefix.sh
@@ -6,3 +6,6 @@ for f in *.TIF; do
 	convert "$f" "$PREFIX$(basename "$f" .TIF).jpg"
 	rm $f
 done
+
+# to prefix 3 digits with 0 :
+# In Total Commander : Select all files > Files > Multi-Rename Tool > Search for : '_(\d{3})\.' Replace With : '_0$1.' Check E and regEx > Start!
