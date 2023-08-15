530286f88e84a19ba9cb93adceb747fe93fd3b0e
Vincent Le Falher <vincent.le.falher@umontreal.ca>
2023-08-15 09:15:57 -0400

    v3.0.9 v3.0-for-ivado-multienv vincent.le.falher@umontreal.ca 2023-08-15 - (container_init.sh, aide_backup_data.sh) backup folder is /home/aide/backup/aidev3

--- docker/container_init.sh
+++ docker/container_init.sh
@@ -54,9 +54,10 @@ sudo systemctl enable postgresql.service
 sudo service postgresql start
 
 # setup the cronjob to backup the database
-mkdir -p /home/aide/backup
+mkdir -p /home/aide/backup/aidev3 # mapped to /mnt/app/backup/aidev3
 echo AIDE_ENV=${AIDE_ENV} > /home/aide/app/aide_env.sh
 (sudo crontab -u root -l 2>/dev/null; echo "0 2 * * * /bin/bash /usr/local/sbin/aide_backup_data.sh 2>&1 | tee /var/log/aide_backup_data.sh-\$(date +\%Y\%m\%dT\%H\%M\%S).log") | sudo crontab -u root -
+(sudo crontab -u root -l 2>/dev/null; echo "30 0 * * * find /var/log/aide_backup_data.sh-*.log -type f -mtime +15 -exec rm -f {} +") | sudo crontab -u root -
 
 echo "=============================="
 echo "Setup of database IS COMPLETED"
--- docker/docker_versions.sh
+++ docker/docker_versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # used to build the docker; only major.minor version. revision/buildnumber should not appear or new docker image will be build as it's named/tagged with version
-AIDE_APP_VERSION=v3.0.8
+AIDE_APP_VERSION=v3.0.9
 DOCKER_AIDE_APP_VERSION=v3_0
 DOCKER_AIDE_VOLUME_VERSION=v_aidev3_0
 AUTODEPLOY=true
