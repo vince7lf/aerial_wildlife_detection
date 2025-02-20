b666531b0d29c7d20791151b1ed59406208848eb
Vincent Le Falher <vincent.le.falher@umontreal.ca>
2025-02-20 09:53:39 -0500

    update backup.md doc

--- scripts/sh/dump_schemas.sh
+++ scripts/sh/dump_schemas.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# sudo /bin/bash /app/aerial_wildlife_detection/scripts/sh/dump_schema.sh | sudo tee /var/log/dump_schema_$(date +\%Y\%m\%dT\%H\%M).log 2>&1
+# Run script as user postgres
+# sudo -u postgres /bin/bash /app/aerial_wildlife_detection/scripts/sh/dump_schema.sh | sudo tee /var/log/dump_schema_$(date +\%Y\%m\%dT\%H\%M).log 2>&1
+# crontab -u postgres -e
 # 0 6 * * * /bin/bash /app/aerial_wildlife_detection/scripts/sh/dump_schema.sh > /var/log/dump_schema_$(date +\%Y\%m\%dT\%H\%M).log 2>&1
-
 set -x
 set -e
 set -u
@@ -24,7 +25,7 @@ sql_query="SELECT schema_name
              AND schema_name != 'public'
              AND schema_name != 'information_schema';"
 
-mapfile -t schemas < <(sudo -u postgres psql -d "$db_name" -t -c "$sql_query" |
+mapfile -t schemas < <(psql -d "$db_name" -t -c "$sql_query" |
                        awk '{gsub(/[ \t]/, ""); if (length($0) > 0) print}')
 
 # Iterate over schemas and perform backups
@@ -37,7 +38,7 @@ for schema in "${schemas[@]}"; do
 
     # Perform the backup
     echo "Backing up schema: $schema"
-    sudo -u postgres pg_dump -Fc -d "$db_name" --schema="\"$schema\"" > "$output_dir/tes2-arbutus-$db_name-$schema-$timestamp.dump"
+    pg_dump -Fc -d "$db_name" --schema="\"$schema\"" > "$output_dir/tes2-arbutus-$db_name-$schema-$timestamp.dump"
 
 done
 
