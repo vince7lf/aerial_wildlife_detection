f01861ed32f514e6b83f29d445bbaa8f3dc9db8c
Vincent Le Falher <vincent.le.falher@umontreal.ca>
2025-04-04 14:47:24 -0400

    fix vascan_id

--- scripts/py/labels_asjson.py
+++ scripts/py/labels_asjson.py
@@ -3,8 +3,11 @@ import json
 
 # Specify the input CSV file and the output JSON file
 # csv_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/etiquettes_annotation_selection_CPK_CLG_serie1_vascan.csv'  # Update this to the name of your CSV file
-csv_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/backup/CoveyHill_139_87_H03_labelclass_20250127_50.csv'  # Update this to the name of your CSV file
-json_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/MELCC-Res-Suivi-BdQc-Volet-4-labels_v20250121.json'
+# csv_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/backup/CoveyHill_139_87_H03_labelclass_20250127_50.csv'  # Update this to the name of your CSV file
+# json_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/MELCC-Res-Suivi-BdQc-Volet-4-labels_v20250121.json'
+
+csv_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/etiquettes_annotation_selection_serie3_20250404_vlf_ready_fixed.csv'  # Update this to the name of your CSV file
+json_file_path = '/mnt/c/Users/vincent.le.falher/Downloads/AIDE+MELCC/MELCC-Res-Suivi-BdQc-Volet-4/etiquettes_annotation_selection_serie3_20250404.json'
 
 # Define a list to hold the processed data
 species_list = []
@@ -15,14 +18,14 @@ with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
 
     for row in csv_reader:
         # Skip rows with missing required fields
-        if not row['name'] or not row['vascan_id']:
+        if not row['name']:
             continue
 
         # Create a dictionary for each species
         species_dict = {
             "name": row["name"],
             "color": "#095797",
-            "vascan_id": row["vascan_id"],
+            "vascan_id": row["vascan_id"] or "-1",
             "coleo_vernacular_fr": row["coleo_vernacular_fr"] or "NA",
             "coleo_vernacular_en": row["coleo_vernacular_en"] or "NA",
             "vascan_port": row["vascan_port"] or "NA",
